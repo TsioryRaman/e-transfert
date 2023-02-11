d749b690031f07379acf2aefaf1a6f44e94d557f
Sitrakiniaina <Sitrakaa9@gmail.com>
2023-02-11 10:04:39 +0300

    feat:validator file

--- src/file-transfert/filetransfer.controller.ts
+++ src/file-transfert/filetransfer.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, ParseIntPipe, Post, Req, Request, UploadedFile, UseGuards, UseInterceptors, UploadedFiles } from '@nestjs/common';
+import { Controller, Body, ParseIntPipe, Post, Req, Request, UploadedFile, UseGuards, UseInterceptors, UploadedFiles, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
 import { AnyFilesInterceptor, FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { FileTransfertService } from './service/file-transfert.service';
@@ -13,7 +13,12 @@ export class FiletransferController {
     SendFile(
         @Body() body,
         @Request() req,
-        @UploadedFiles() files: Array<Express.Multer.File>
+        @UploadedFiles(new ParseFilePipe({
+            validators: [
+                new MaxFileSizeValidator({ maxSize: 1000000 }),
+                // new FileTypeValidator({ fileType:  }),
+            ],
+        })) files: Array<Express.Multer.File>
     ) {
         console.log(files);
         return this.fileTransfertService.SendFile(1, body.idReceiver, files)
